91b22ca23a91d9637350e1e92c2a83b4cf364f97
1conorsteward <1conorsteward@gmail.com>
2024-09-11 09:15:26 -0700

    Add module2 with MVC architecture and templating engine setup

--- Downloads/travlr/app.js
+++ Downloads/travlr/app.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -19,8 +20,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Static file serving the middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
+
+//Registering hbs
+var hbs = require('hbs');
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views', 'partials'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
